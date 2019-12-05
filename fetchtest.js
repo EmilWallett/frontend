722c0e9096453f55dab82da27db0fc211ca414db
M4DMK <58467453+M4DMK@users.noreply.github.com>
2019-12-05 09:54:21 +0100

    Delete fetchtest.js

--- fetchtest.js
+++
@@ -1,5 +0,0 @@
-const getPosts = () => {
-  return fetch(`http://jsonplaceholder.typicode.com/posts`) /* <--- Put URL of Website in here*/
-  .then(res => res.json())
-  .then(posts => console.log(posts))
-}
