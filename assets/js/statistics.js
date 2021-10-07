138914c335176087c4b35bf93f22e12f6b43920a
Marius Bauerschmidt <58471992+INFGMYoshi@users.noreply.github.com>
2021-10-07 11:20:35 +0200

    Changing statistic.js
    Removing and adding connecting
    to have less inline script

--- assets/js/statistics.js
+++ assets/js/statistics.js
@@ -1,2 +1,12 @@
 var webbServerIp = "http://its.teknikum.it:8080/", serverPath = "sustaining_backend/api/";
 var webbServerAdress = webbServerIp + serverPath;
+
+var data = {
+  nrOfPictures: 2500,
+  nrOfVotes: 3400,
+  nrOfFames: 1500,
+  nrOfShames: 1000,
+  nrOfUsers: 112,
+  percentageOfFame: 0,
+  percentageOfShame: 0
+};
