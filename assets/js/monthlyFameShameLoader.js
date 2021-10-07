a58c9d29229185e1c1b6c3c151a308183a921379
Loke Lindberg Nilsson <66024236+Lokecrafter@users.noreply.github.com>
2021-10-07 15:48:32 +0200

    Loader fetching random data

--- assets/js/monthlyFameShameLoader.js
+++ assets/js/monthlyFameShameLoader.js
@@ -4,7 +4,7 @@ var shameContainer = document.getElementById("shameImages");
 window.onload = function(){
 
     return fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(Response => Response.json)
+        .then(Response => Response.json())
         .then(data => {
             data.forEach(element => {
                 console.log(element.id);
