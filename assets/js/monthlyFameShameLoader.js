ace88810a534749dc25174ea9ea52f1c4c5fb426
Loke Lindberg Nilsson <66024236+Lokecrafter@users.noreply.github.com>
2021-10-07 16:00:17 +0200

    Added more lines to monthly loader

--- assets/js/monthlyFameShameLoader.js
+++ assets/js/monthlyFameShameLoader.js
@@ -4,6 +4,10 @@ var shameContainer = document.getElementById("shameImages");
 var fImgs = [fameContainer.firstElementChild, fameContainer.lastElementChild]
 var sImgs = [shameContainer.firstElementChild, shameContainer.lastElementChild]
 
+console.log(fImgs[0].tagName);
+console.log(fImgs[1].tagName);
+
+
 window.onload = function(){
     return fetch('https://jsonplaceholder.typicode.com/posts')
         .then(Response => Response.json())
@@ -11,6 +15,14 @@ window.onload = function(){
             data.forEach(element => {
                 console.log(element.id);
                 console.log(element.title);
+
+                return; //stops the function from executing any more code
+
+                fImgs[0].href = data.firstFame;
+                fImgs[1].href = data.secondFame;
+
+                sImgs[0].href = data.firstShame;
+                sImgs[1].href = data.secondShame;
             });
         });
 };
