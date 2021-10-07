000523a402946f403cdcf92c90e1a4fd28b02d54
Loke Lindberg Nilsson <66024236+Lokecrafter@users.noreply.github.com>
2021-10-07 15:46:38 +0200

    Start work on monthly loader

---
+++ assets/js/monthlyFameShameLoader.js
@@ -0,0 +1,16 @@
+var fameContainer = document.getElementById("fameImages");
+var shameContainer = document.getElementById("shameImages");
+
+window.onload = function(){
+
+    return fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(Response => Response.json)
+        .then(data => {
+            data.forEach(element => {
+                console.log(element.id);
+                console.log(element.title);
+            });
+        });
+
+
+};
