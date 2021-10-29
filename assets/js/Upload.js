d1ad45e673630240207a165745b3c5d17ce5112d
eemmiiee <emmievolcsey@yahoo.com>
2021-10-29 10:01:03 +0200

    js changes

--- assets/js/Upload.js
+++ assets/js/Upload.js
@@ -2,15 +2,20 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.querySelectorAll('button');
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+for(let i = 0; i < btn.length; i++){
+  btn[i].addEventListener("click", () => {
+    modal.style.display = "block";
+  })
 }
+
+
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
