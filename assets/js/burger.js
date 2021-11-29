f81d10f46a4484d0c5b8882785266316a5e6c1ba
EmilWallett <89247603+EmilWallett@users.noreply.github.com>
2021-11-29 14:55:16 +0100

    added burger menu to the mobile version

---
+++ assets/js/burger.js
@@ -0,0 +1,9 @@
+function toggleBurger() {
+    var x = document.getElementById("myLinks");
+    console.log(x)
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
